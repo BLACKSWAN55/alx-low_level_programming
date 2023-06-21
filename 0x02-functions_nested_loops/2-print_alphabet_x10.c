1f6a024dfc6c3a1d2872f4c50a6baf8b1e3a0d7f
BLACKSWAN55 <ememercy6@gmail.com>
2023-06-21 07:59:53 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,10 +13,11 @@ void print_alphabet_x10(void)
 	while (i < 10)
 	{
 
-	for (ch = 'a'; c <= 'z'; ch++)
-	{
-	_putchar(ch);
-	_putchar('\n');
-	i++;
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+		_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
 	}
 }
