e016feb85dff54bab5cd372b1aedeb31be88a910
BLACKSWAN55 <ememercy6@gmail.com>
2023-07-21 08:56:18 -0700

    update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -29,12 +29,11 @@ int op_sub(int a, int b)
 	return (a - b);
 }
 /**
- 21  * op_mul - Returns the different of two numbers
- 22  * @a: The first number.
- 23  * @b: The second number.
- 24  *
- 25  * Return: The mul of a and b.
- 26  */
+ * op_mul - Returns the different of two numbers
+ * @a: The first number.
+ * @b: The second number.
+ * Return: The mul of a and b.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
